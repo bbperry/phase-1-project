d7f80f2bf73f8273126c7df3be7ac0af08bb12a6
Ben <benbperry1@gmail.com>
2021-11-15 14:57:28 -0800

    update before branch

--- src/index.js
+++ src/index.js
@@ -1,35 +1,40 @@
-const random = 'https://www.themealdb.com/api/json/v1/1/random.php';
     const preview = document.getElementById('preview');
     const previewImage = document.createElement('img');
     const mealName = document.getElementById('mealName')
     const mealNameInsert = document.createElement('h2')
     const instructions = document.getElementById('instructions')
     const instructionsInsert = document.createElement('p')
-    
+    const bigPicture = document.getElementById('bigPicture')
+    const bigPictureInsert = document.createElement('img')
+    const random = document.getElementById('random')
     
     
 // Random Meal
+random.addEventListener('click', () => {   
 fetch('https://www.themealdb.com/api/json/v1/1/random.php')
   .then((resp) => resp.json())
   .then(data => renderPreview(data.meals[0]));
-
-
 
 function renderPreview(meals) {
   // const mealData = meals.meals[0]  
   previewImage.id='previewImage'
 
-  mealNameInsert.innerText = meals.strMeal
-  mealName.append(mealNameInsert)
+  
 
   previewImage.src = meals.strMealThumb
   preview.append(previewImage);
   console.log(previewImage);
 
+  previewImage.addEventListener('click', () => {
+  
+  mealNameInsert.innerText = meals.strMeal
+  mealName.append(mealNameInsert)
   instructionsInsert.innerText = meals.strInstructions
   instructions.append(instructionsInsert)
 
- 
+  bigPictureInsert.src = meals.strMealThumb
+  bigPictureInsert.id = 'bigPictureInsert'
+  bigPicture.append(bigPictureInsert)
    
     for (let i = 0; i <= 20; i++) {
       const ingredients = document.getElementById('ingredients')
@@ -37,8 +42,9 @@ function renderPreview(meals) {
         addIngredient.innerText = meals.strIngredient1[i]
         ingredients.appendChild(addIngredient)
       } 
-  }
-
+  })
+}
+})
 
 
 // Meal Search
